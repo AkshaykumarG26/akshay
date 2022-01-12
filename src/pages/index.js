26054d2167d4fce7d304ffdbec7d8381135f2c20
AkshaykumarG26 <akshayg2697@gmail.com>
2022-01-12 20:33:05 +0530

    .

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ import { Section } from '../styles/GlobalComponents';
 const Home = () => {
   return (
     <Layout>
-      <Section flex>
+      <Section grid>
         <Hero />
         <BgAnimation />
       </Section>
