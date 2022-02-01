7677fa7fa7617a9436ee4f366625393be313b3f0
Akshaykumar V. Gullapalli <akshayg2697@gmail.com>
2022-02-01 16:19:41 +0530

    .

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -2,7 +2,7 @@ export const projects = [
   {
     title: "Practo.com",
     description:
-      "In this project we have tried to enhance the cloning of “Practo” website using frontend with React Js, Chakra UI, Bootstrap, redux and backend with validations, authentications and more.",
+      "In this project we have cloned “Practo” website using frontend with React Js, Chakra UI, Bootstrap, redux and backend with validations, authentications and more.",
     image: "/images/1.png",
     tags: ["ReaactJS", "ChakraUI", "Bootstrap"],
     source: "https://github.com/nileshagrawal98/Practo_Clone",
@@ -12,7 +12,7 @@ export const projects = [
   {
     title: "Groww.in",
     description:
-      "In this project we have created user interface of groww webpage and added backend. Here user is able to see all the stocks and buy them, sell them and delete the stocks from cart.",
+      "In this project we have created user interface of groww webpage and added backend. Here user is able to see all the stocks and buy them, sell and delete stocks from cart.",
     image: "/images/2.png",
     tags: ["NodeJS", "ExpressJS", "MongoDB"],
     source: "https://github.com/taherahmed14/Groww_Full-Stack_Project",
